6207e94a7fcdc82edf2b7e5d147e0a49225b6e7f
ckodser <arshia.soltani2@gmail.com>
2021-01-13 14:52:05 +0330

    write restaurant_query_handeler.py debug delivery_query_handeler.py

---
+++ restaurant_query_handeler.py
@@ -0,0 +1,36 @@
+import psycopg2
+import json
+import Id_handler
+
+f = open('config_file.JSON')
+data = json.load(f)
+con = psycopg2.connect(database=data["postgresql"]["database"], user=data["postgresql"]["user"],
+                       password=data["postgresql"]["password"], host=data["postgresql"]["host"],
+                       port=data["postgresql"]["port"])
+cur = con.cursor()
+
+
+def food_is_ready(order_id):
+    cur.execute('''UPDATE RESorder SET preparingTime = CURRENT_TIMESTAMP WHERE ordertId=''' + str(order_id))
+
+
+def get_not_completed_order(restaurant_id):
+    cur.execute('''SELECT id from Resorder WHERE restaurantId=''' + str(restaurant_id) + '''
+    AND preparingTime IS NULL'''
+                )
+    return cur.fetchall()
+
+
+def set_delivery_for_order(order_id, delivery_id):
+    cur.execute('''INSERT INTO Ressending(orderId,deliveryId) VALUES(''' + str(order_id) + "," + str(delivery_id) + ")")
+
+
+def increase_amount(food_id, amount):
+    cur.execute('''SELECT amount from Resfood WHERE Id=''' + str(food_id) + ")")
+    rows = cur.fetchall()
+    amount += rows[0][0]
+    cur.execute('''UPDATE Resfood SET amount=''' + str(amount) + ''' WHERE Id=''' + str(food_id) + ")")
+
+
+con.commit()
+con.close()
