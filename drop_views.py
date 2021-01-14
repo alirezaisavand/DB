abaf0a05d22b76470d3e385284289e42ecdbb9b5
alirezaisavand <isavand.alireza@gmail.com>
2021-01-14 21:19:11 +0330

    drops and creates work well!

--- drop_views.py
+++ drop_views.py
@@ -7,26 +7,27 @@ con = psycopg2.connect(database=data["postgresql"]["database"], user=data["postg
 
 cur = con.cursor()
 
-cur.execute('''drop view CusfoodOrdered''')
 cur.execute('''drop view Cusfood''')
-cur.execute('''drop view Cussending''')
-cur.execute('''drop view Cusorder''')
 cur.execute('''drop view Cusrestaurant''')
 cur.execute('''drop view Cusdelivery''')
 cur.execute('''drop view CusdiscountCode''')
-cur.execute('''drop view Cusbasket''')
 cur.execute('''drop view Cuscustomer''')
+cur.execute('''drop view Cusbasket''')
+cur.execute('''drop view Cusorder''')
+cur.execute('''drop view Cussending''')
+cur.execute('''drop view CusfoodOrdered''')
 
 print("Drop all Customer views Done!")
 
 con.commit()
 
-cur.execute('''drop view Delsending''')
-cur.execute('''drop view Delorder''')
 cur.execute('''drop view Delrestaurant''')
 cur.execute('''drop view Deldelivery''')
 cur.execute('''drop view Delcustomer''')
+cur.execute('''drop view Delorder''')
+cur.execute('''drop view Delsending''')
 
+con.commit()
 print("Drop all delivery views Done!")
 
 cur.execute('''drop view Resrestaurant''')
@@ -38,5 +39,5 @@ cur.execute('''drop view Ressending''')
 cur.execute('''drop view ResfoodOrdered''')
 
 con.commit()
-
+print("Drop all restaurant views Done!")
 con.close()
