180f59b6f3e6e3b5032dd28a87e63b2652eed461
Shayan_Jahan <shayantec95@gmail.com>
2021-01-30 20:45:00 +0330

    Add sign_up to delivery_graphic.py

--- delivery_graphic.py
+++ delivery_graphic.py
@@ -1,6 +1,12 @@
 import PySimpleGUI as sg
 import delivery_query_handeler
 
+def check_complete(values):
+    for i in range(len(values)):
+        if values[i].strip() == '':
+            return False
+    return True
+
 def home_page(id):
     print(id)
 
@@ -16,6 +22,40 @@ def home_page(id):
         if event == "Back":
             window.close()
             return
+
+def sign_up():
+    incomplete = 0
+    while True:
+        layout = [
+            [sg.Text('Enter Name',size=(20, 1)), sg.InputText()],
+            [sg.Text('Enter Salary',size=(20, 1)), sg.InputText()],
+            [sg.Text('Enter Area',size=(20, 1)), sg.InputText()],
+            [sg.Button('OK',size=(10, 1)), sg.Button("Back",size=(10, 1))]
+        ]
+
+        if incomplete:
+            layout.append([sg.Text('Please Fill Inputs', text_color='red')])
+
+        window = sg.Window("Delivery App", layout)
+
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            exit(0)
+
+        elif event == "Back":
+            window.close()
+            return
+
+        elif event == "OK":
+            if check_complete(values):
+                incomplete = 0
+                delivery_query_handeler.add_new_delivery(values[0], values[1], values[2])
+                print("Delivery Added")
+                window.close()
+                return
+            else:
+                incomplete = 1
+            window.close()
 
 def initial_screen():
     wrong_info = 0
@@ -34,14 +74,14 @@ def initial_screen():
             exit(0)
 
         if event == "OK":
-            my_tOKen = delivery_query_handeler.check_name(values[0])
-            if my_tOKen == -1:
+            my_token = delivery_query_handeler.check_name(values[0])
+            if my_token == -1:
                 print("Wrong Information")
                 wrong_info = 1
             else:
                 wrong_info = 0
                 window.close()
-                home_page(my_tOKen)
+                home_page(my_token)
             window.close()
         elif event == "Sign Up":
             wrong_info = 0
